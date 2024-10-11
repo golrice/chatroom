2c7ccfd7beba27e99f63c224e70c07093f37ecf5
golrice <791554926@qq.com>
2024-10-11 15:12:42 +0800

    start from begin

--- main.go
+++ main.go
@@ -1,38 +1,7 @@
 package main
 
-import (
-	"log"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
+import "fmt"
 
 func main() {
-	// create a default server
-	ginServer := NewServer("127.0.0.1", 8080)
-	var err error
-	db, err = NewDb("127.0.0.1", 3306, "chatroom")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Database.Close()
-
-	// init files path
-	ginServer.engine.Static("static", "./static")
-	ginServer.engine.LoadHTMLGlob("./templates/*")
-
-	EmailVerification(ginServer.engine)
-
-	LoginPage(ginServer.engine)
-	RegisterPage(ginServer.engine)
-	forgotPasswordPage(ginServer.engine)
-
-	ChatPage(ginServer.engine)
-
-	ginServer.engine.NoRoute(func(ctx *gin.Context) {
-		ctx.HTML(http.StatusNotFound, "404.html", nil)
-	})
-
-	// apply a port for server
-	ginServer.Run()
+    fmt.Println("hello world")
 }
